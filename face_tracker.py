2ff35475023c9a6b7a18a083b7f873f8b1459b2d
Ales Lenko <ales.lenko@student.um.si>
2023-03-14 18:08:56 +0100

    v0.5 - implemented getting color from ROI after it is selected

--- face_tracker.py
+++ face_tracker.py
@@ -1,13 +1,24 @@
 import numpy
 import cv2
 
-# version 0.4
+# version 0.5
 
 def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    # izrežemo iz slike roi, ki smo ga prej izbrali
+    roi = slika[levo_zgoraj[1]:desno_spodaj[1], levo_zgoraj[0]:desno_spodaj[0]] 
+    cv2.imshow("roi test", roi) 
+    # Izracunamo povprecno barvo izbrane slike
+    # Dobimo povprecje navpicnih vrstic 
+    avg_color_per_col = numpy.mean(roi, axis=1)
+    # Preko povprecij navpicnih vrstic nato zracunamo se povprecje vodoravnih vrstic 
+    avg_color = numpy.mean(avg_color_per_col, axis=1) 
+    # Dodamo šum na min/max vrednost meje koze (+/- 10)
+    spodnja_meja_koze = numpy.array((avg_color[0]-10, avg_color[1]-10, avg_color[2]-10))
+    zgornja_meja_koze = numpy.array((avg_color[0]+10, avg_color[1]+10, avg_color[2]+10))
     return (spodnja_meja_koze, zgornja_meja_koze)
 
 def zmanjsaj_sliko(slika):
-    pass
+    return numpy.array(cv2.resize(slika, (260, 300)))
 
 def obdelaj_sliko(slika, okno_sirina, okno_visina, barva_koze_spodaj, barva_koze_zgoraj):
     pass
@@ -28,7 +39,7 @@ if not cap.isOpened():
     
 barva_koze = None
 while True:    
-
+    
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame. Exiting ...")
@@ -36,13 +47,27 @@ while True:
 
     # preslikava posnetka kamere (1 slika) v numpy.array()
     image = numpy.array(frame)
+    resizedImage = zmanjsaj_sliko(image)
     
     # označevanje roi (Region Of Interest), ki se potem uporabi za pridobitev barve kože
+    """
+    cv2.selectROI() - vrne tuple
+       (31, 65, 125, 120)
+        ^   ^    ^    ^
+        |   |    |    |
+        x1  y1   |    y2 = 120 + 65
+                x2 = 125 + 31
+        x - navzdol // y - vodoravno
+        x1,y1 levo zgoraj
+        x2,y2 desno spodaj
+    """
     if cv2.waitKey(1) == ord('r'):
-        roi = cv2.selectROI(frame)
+        roi = cv2.selectROI(resizedImage)
+        barva_koze = doloci_barvo_koze(resizedImage, (roi[0], roi[1]), (roi[2] + roi[0], roi[3] + roi[1]))
+
 
     # prikaz
-    cv2.imshow("\"Face Tracker\"", frame)  
+    cv2.imshow("\"Face Tracker\"", resizedImage)  
 
     # QUIT CAPTURE  
     if cv2.waitKey(1) == ord('q'):
